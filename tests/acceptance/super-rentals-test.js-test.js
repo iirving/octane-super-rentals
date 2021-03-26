8b553c84c73b5a6462ccd263e5f1cba9dddd6e7f
iirving <ian@falsepositives.com>
2021-03-26 10:33:48 -0400

    add and test a NavBar component

--- tests/acceptance/super-rentals-test.js-test.js
+++ tests/acceptance/super-rentals-test.js-test.js
@@ -8,7 +8,9 @@ module('Acceptance | super rentals', function(hooks) {
   test('visiting root', async function(assert) {
     await visit('/');
 
-    assert.equal(currentURL(), '/');
+    assert.equal(currentURL(), '/', 'rigth root path');
+    assert.dom('nav').exists();
+    assert.dom('h1').hasText('Super Rentals');
     assert.dom('h2').hasText('Welcome to Ian\'s Octance Super Rentals!')
   });
 
@@ -17,16 +19,23 @@ module('Acceptance | super rentals', function(hooks) {
 
     assert.equal(currentURL(), '/');
 
+    assert.dom('nav').exists();
+    assert.dom('h1').hasText('Super Rentals');
     assert.dom('.jumbo a.button').hasText('About Us')
     await click('.jumbo a.button')
 
     assert.equal(currentURL(), '/about')
+    assert.dom('nav').exists();
+    assert.dom('h1').hasText('Super Rentals');
+    assert.dom('h2').hasText('About Super Rentals');
   });
 
   test('visting getting-in-touch happy path ', async function(assert) {
     await visit('/getting-in-touch');
 
     assert.equal(currentURL(), '/getting-in-touch');
+    assert.dom('nav').exists();
+    assert.dom('h1').hasText('Super Rentals');
     assert.dom('h2').hasText('Contact Us');
 
     assert.dom('.jumbo a.button').hasText('About Us');
@@ -38,6 +47,5 @@ module('Acceptance | super rentals', function(hooks) {
     await click('.jumbo a.button')
 
     assert.equal(currentURL(), '/getting-in-touch');
-
   })
 });
