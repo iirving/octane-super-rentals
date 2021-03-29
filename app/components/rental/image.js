55e7fb412c534c5a8e2b75bc05a536f54fdba11e
iirving <ian@falsepositives.com>
2021-03-29 16:49:40 -0400

    add interactive behavior to  Rental:Image component

---
+++ app/components/rental/image.js
@@ -0,0 +1,14 @@
+// @ts-ignore
+import Component from '@glimmer/component';
+// @ts-ignore
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+
+export default class RentalImageComponent extends Component {
+
+  @tracked isLarge = false;
+
+  @action toggleSize() {
+    this.isLarge = !this.isLarge;
+  }
+}
