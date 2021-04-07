55436b295d838eba4e26ec1e0d8b5e2fb5220a21
iirving <ian@falsepositives.com>
2021-04-07 14:15:49 -0400

    config MAPBOX_ACCESS_TOKEN via .env

--- config/dotenv.js
+++ config/dotenv.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 module.exports = function (/* env */) {
   return {
-    clientAllowedKeys: [MAPBOX_ACCESS_TOKEN],
+    clientAllowedKeys: ['MAPBOX_ACCESS_TOKEN'],
     fastbootAllowedKeys: [],
     failOnMissingKey: false,
     path: path.join(path.dirname(__dirname), '.env')
